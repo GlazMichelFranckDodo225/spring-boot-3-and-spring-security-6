2c551aa453ab7ac29415181e6bc7b0a58c6e0232
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-02 22:16:49 +0200

    Creating a Simple Spring Boot App without Spring Security

---
+++ src/main/java/com/dgmf/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.dgmf.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application without Spring Security";
+    }
+}
