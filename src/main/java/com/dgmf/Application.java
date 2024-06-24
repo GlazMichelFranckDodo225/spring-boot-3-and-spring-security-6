88b2f397324460db92839976e2c9182c00360066
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-24 08:26:38 +0200

    Creating Backend Services Needed for the EazyBank Application

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -2,11 +2,8 @@ package com.dgmf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// Optional ==> Because SubPackage of Main Class Package
-@ComponentScan("com.dgmf.controller")
 public class Application {
 
 	public static void main(String[] args) {
